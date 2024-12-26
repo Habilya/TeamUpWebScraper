be92ec4a83ce7009909bf66256769c6521050a0e
Habilya <ciuvilin.co@gmail.com>
2024-12-26 12:21:33 -0500

    Add InputValidation Dates and tests

---
+++ src/TeamUpWebScraperLibrary/Validators/InputValidation.cs
@@ -0,0 +1,50 @@
+﻿using TeamUpWebScraperLibrary.Providers;
+
+namespace TeamUpWebScraperLibrary.Validators;
+
+public class InputValidation
+{
+	private readonly IDateTimeProvider _dateTimeProvider;
+
+	public InputValidation(IDateTimeProvider dateTimeProvider)
+	{
+		_dateTimeProvider = dateTimeProvider;
+	}
+
+	public List<string> ValidateDatesRange(DateTime? dateFrom, DateTime? dateTo)
+	{
+		var validationMessages = new List<string>();
+
+		if (dateFrom is null)
+		{
+			validationMessages.Add("DateFrom cannot be empty");
+		}
+
+		if (dateTo is null)
+		{
+			validationMessages.Add("DateTo cannot be empty");
+		}
+
+		if (dateFrom > dateTo)
+		{
+			validationMessages.Add("DateFrom cannot be greater than DateTo");
+		}
+
+		if (dateTo is not null && dateFrom is not null)
+		{
+			var monthsSpan = (dateTo.Value.Month - dateFrom.Value.Month) + 12 * (dateTo.Value.Year - dateFrom.Value.Year);
+			if (monthsSpan > 2)
+			{
+				validationMessages.Add("Span of DateFrom to DateTo cannot be greater than 2 months (Too much Data)");
+			}
+		}
+
+		var currentDateTime = _dateTimeProvider.DateTimeNow;
+		if (dateFrom > currentDateTime)
+		{
+			validationMessages.Add($"DateFrom may not be in the future (greater than {currentDateTime.ToString("yyyy-MM-dd")})");
+		}
+
+		return validationMessages;
+	}
+}
---
+++ test/TeamUpWebSraperLibrary.Tests.Unit/InputValidationTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using TeamUpWebScraperLibrary.Providers;
+using TeamUpWebScraperLibrary.Validators;
+
+namespace TeamUpWebSraperLibrary.Tests.Unit;
+
+public class InputValidationTests
+{
+	[Theory]
+	// Validation should fail when either of the input dates is null
+	[InlineData(1, null, null)]
+	[InlineData(2, "2022-02-02", null)]
+	// Validation should fail when date From is greater than DateTo
+	[InlineData(3, "2022-01-05", "2022-01-02")]
+	// Span of DateFrom to DateTo cannot be greater than 2 months
+	[InlineData(4, "2022-02-02", "2022-05-02")]
+	// DateFrom may not be in the future
+	[InlineData(5, "2022-03-02", "2022-03-20")]
+	// All validations are met, this should pass
+	[InlineData(6, "2022-02-01", "2022-02-25")]
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters", Justification = "UnitTests with testId")]
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "UnitTests with testId")]
+	public void ValidateDatesRange_ShouldReturnValidationMessages_WhenInputIncorrect(int id, string? dateFromAsString, string? dateToAsString)
+	{
+		// Arrange
+		IDateTimeProvider dateTimeProvider = Substitute.For<IDateTimeProvider>();
+		dateTimeProvider.DateTimeNow.Returns(new DateTime(2022, 2, 2, 20, 0, 0));
+
+		var _sut = new InputValidation(dateTimeProvider);
+
+		const string DateInputFormat = "yyyy-MM-dd";
+		DateTime? dateFrom;
+		DateTime? dateTo;
+		dateFrom = ParseDateForTest(dateFromAsString, DateInputFormat);
+		dateTo = ParseDateForTest(dateToAsString, DateInputFormat);
+
+		var dataMap = new Dictionary<int, List<string>>
+		{
+			{1, new List<string> {"DateFrom cannot be empty", "DateTo cannot be empty"}},
+			{2, new List<string> {"DateTo cannot be empty"}},
+			{3, new List<string> {"DateFrom cannot be greater than DateTo"}},
+			{4, new List<string> {"Span of DateFrom to DateTo cannot be greater than 2 months (Too much Data)"}},
+			{5, new List<string> {"DateFrom may not be in the future (greater than 2022-02-02)"}},
+			{6, new List<string> { }},
+		};
+
+		var expected = dataMap[id];
+
+
+		// Act
+		var actual = _sut.ValidateDatesRange(dateFrom, dateTo);
+
+		// Assert
+		actual.Should().BeEquivalentTo(expected);
+	}
+
+	private static DateTime? ParseDateForTest(string? dateAsString, string DateInputFormat)
+	{
+		if (string.IsNullOrWhiteSpace(dateAsString))
+		{
+			return null;
+		}
+
+		DateTime.TryParseExact(dateAsString, DateInputFormat, null, System.Globalization.DateTimeStyles.None, out DateTime dateFrom);
+		return dateFrom;
+	}
+}
