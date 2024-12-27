05132398215a8e15a7ed0033e753cc6de9958cd8
Habilya <ciuvilin.co@gmail.com>
2024-12-26 19:22:01 -0500

    Add calendar config models

---
+++ src/TeamUpWebScraperLibrary/TeamUpAPI/Models/Config/CalendarConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamUpWebScraperLibrary.TeamUpAPI.Models.Config;
+
+public class CalendarConfiguration
+{
+	public long Id { get; set; } = default!;
+	public string Name { get; set; } = default!;
+	public bool Active { get; set; } = default!;
+	public int Color { get; set; } = default!;
+	public bool Overlap { get; set; } = default!;
+	public bool Readonly { get; set; } = default!;
+	public DateTime CreationDt { get; set; } = default!;
+	public DateTime UpdateDt { get; set; } = default!;
+}
