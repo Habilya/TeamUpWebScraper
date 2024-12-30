623e5ef18589f2b3ec03786cb415fe3c63f56336
Habilya <ciuvilin.co@gmail.com>
2024-12-29 19:06:45 -0500

    Versionning adjustment

--- src/TeamUpWebScraperUI/Constants/Versionning.cs
+++ src/TeamUpWebScraperUI/Constants/Versionning.cs
@@ -1,4 +1,7 @@
-﻿namespace TeamUpWebScraperUI.Constants;
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace TeamUpWebScraperUI.Constants;
 
 public static class Versionning
 {
@@ -6,23 +9,51 @@ public static class Versionning
 	public const string ENV_DOTNET_ENVIRONMENT = "DOTNET_ENVIRONMENT";
 	public const string ENV_DEFAULT_VARIABLE_ANSWER = "N/A";
 
-	public static string GetVersion()
-	{
-		return typeof(Program).Assembly.GetName().Version?.ToString() ?? ENV_DEFAULT_VARIABLE_ANSWER;
-	}
 
-	public static string GetBuildNumber()
+	#region I couldn't get ENV Variables to work...
+	private static string GetBuildNumber()
 	{
 		return Environment.GetEnvironmentVariable(ENV_BUILD_NUMBER) ?? ENV_DEFAULT_VARIABLE_ANSWER;
 	}
 
-	public static string GetRunningEnvironement()
+	private static string GetRunningEnvironement()
 	{
 		return Environment.GetEnvironmentVariable(ENV_DOTNET_ENVIRONMENT) ?? ENV_DEFAULT_VARIABLE_ANSWER;
+	}
+	#endregion
+
+	public static string GetVersion()
+	{
+		try
+		{
+			var roughVersion = typeof(Program).Assembly.GetName().Version?.ToString();
+			var version = Regex
+				.Match(roughVersion!, @"^([0-9]*.[0-9]*.[0-9]*)(?=.[0-9]*)")
+				.ToString();
+			return version;
+		}
+		catch (Exception)
+		{
+			return ENV_DEFAULT_VARIABLE_ANSWER;
+		}
+	}
+
+	private static string GetVersionPostfix()
+	{
+		try
+		{
+			return Assembly.GetEntryAssembly()!
+				.GetCustomAttribute<AssemblyFileVersionAttribute>()!
+				.Version;
+		}
+		catch (Exception)
+		{
+			return ENV_DEFAULT_VARIABLE_ANSWER;
+		}
 	}
 
 	public static string GetVersionOneLiner()
 	{
-		return $"v{GetVersion()}-{GetBuildNumber()} env: {GetRunningEnvironement()}";
+		return $"v{GetVersion()}-{GetVersionPostfix()}";
 	}
 }
