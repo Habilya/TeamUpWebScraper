7c767e440a02baca541eee760f253395c4c40d10
Habilya <ciuvilin.co@gmail.com>
2025-01-16 11:39:24 -0500

    clean usings

--- src/TeamUpWebScraperLibrary/DependencyInjection.cs
+++ src/TeamUpWebScraperLibrary/DependencyInjection.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
-
 using TeamUpWebScraperLibrary.DisplayGridView;
 using TeamUpWebScraperLibrary.ExcelSpreadsheetReport;
 using TeamUpWebScraperLibrary.ExcelSpreadsheetReport.Models;
